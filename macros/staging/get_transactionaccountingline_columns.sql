f4c05f39aed763ee99aae00655985555ac9f8e0c
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:40:56 +1000

    Update get_transactionaccountingline_columns.sql

--- macros/staging/get_transactionaccountingline_columns.sql
+++ macros/staging/get_transactionaccountingline_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_transactionaccountingline_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "account", "datatype": dbt.type_int()},
     {"name": "accountingbook", "datatype": dbt.type_int()},
@@ -29,7 +29,7 @@
 {% macro get_netsuite2_transactionaccountingline_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "account", "datatype": dbt.type_int()},
     {"name": "accountingbook", "datatype": dbt.type_int()},
