0519dedb14f85fe6aa617c297842b941e8b115e9
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:32:18 +1000

    Update get_entity_columns.sql

--- macros/staging/get_entity_columns.sql
+++ macros/staging/get_entity_columns.sql
@@ -2,7 +2,7 @@
 
 {% set columns = [
     {"name": "_fivetran_active", "datatype": dbt.type_boolean()},
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "contact", "datatype": dbt.type_int()},
     {"name": "customer", "datatype": dbt.type_int()}, 
