7d072bc0839f6181c22d62a3f3f0aef3985b7c71
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:30:13 +1000

    Update get_customer_subsidiary_relationships_columns.sql

--- macros/staging/get_customer_subsidiary_relationships_columns.sql
+++ macros/staging/get_customer_subsidiary_relationships_columns.sql
@@ -2,7 +2,7 @@
 
 {% set columns = [
     {"name": "_fivetran_active", "datatype": dbt.type_boolean()},
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "balance", "datatype": dbt.type_float()},
     {"name": "date_deleted", "datatype": dbt.type_timestamp()},
