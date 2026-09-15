20fa783f55c4e744bc2c49ace9c775c6ef28796d
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:43:00 +1000

    Update get_vendorcategory_columns.sql

--- macros/staging/get_vendorcategory_columns.sql
+++ macros/staging/get_vendorcategory_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_vendorcategory_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "date_deleted", "datatype": dbt.type_timestamp()},
     {"name": "externalid", "datatype": dbt.type_string()},
@@ -20,7 +20,7 @@
 
 {% set columns = [
     {"name": "_fivetran_active", "datatype": dbt.type_boolean()},
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "id", "datatype": dbt.type_int()},
     {"name": "name", "datatype": dbt.type_string()}
