2d723e55292c3d721c646247a1befadb16c5826a
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:26:29 +1000

    Update get_accountingperiodfiscalcalendars_columns.sql

--- macros/staging/get_accountingperiodfiscalcalendars_columns.sql
+++ macros/staging/get_accountingperiodfiscalcalendars_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_accountingperiodfiscalcalendars_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_id", "datatype": dbt.type_string()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "accountingperiod", "datatype": dbt.type_int()},
@@ -17,7 +17,7 @@
 {% macro get_netsuite2_accountingperiodfiscalcalendars_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_id", "datatype": dbt.type_string()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "accountingperiod", "datatype": dbt.type_int()},
