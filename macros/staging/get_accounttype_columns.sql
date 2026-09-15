d41849d5e792c0272a05b2413d759fec2fde0a61
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:27:47 +1000

    Update get_accounttype_columns.sql

--- macros/staging/get_accounttype_columns.sql
+++ macros/staging/get_accounttype_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_accounttype_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "balancesheet", "datatype": dbt.type_string()},
     {"name": "date_deleted", "datatype": dbt.type_timestamp()},
@@ -24,7 +24,7 @@
 {% macro get_netsuite2_accounttype_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "balancesheet", "datatype": dbt.type_string()},
     {"name": "id", "datatype": dbt.type_string()},
