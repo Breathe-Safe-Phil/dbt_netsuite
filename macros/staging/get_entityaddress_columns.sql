024a8ad1960445a422217312161c391f7b7638ad
Breathe-Safe-Phil <p.robinson@breathe-safe.com>
2026-09-15 13:32:49 +1000

    Update get_entityaddress_columns.sql

--- macros/staging/get_entityaddress_columns.sql
+++ macros/staging/get_entityaddress_columns.sql
@@ -1,7 +1,7 @@
 {% macro get_entityaddress_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "addr1", "datatype": dbt.type_string()},
     {"name": "addr2", "datatype": dbt.type_string()},
@@ -30,7 +30,7 @@
 
 {% set columns = [
     {"name": "_fivetran_active", "datatype": dbt.type_boolean()},
-    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "addr1", "datatype": dbt.type_string()},
     {"name": "addr2", "datatype": dbt.type_string()},
